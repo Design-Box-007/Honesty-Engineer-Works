a01cca7635455ceb56a895db09b003b07ffd075a
Akram-00 <aliakram9789@gmail.com>
2025-02-13 12:31:20 +0530

    vite config is updated

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/HonestyEngineeringWorks/" 
-})
+  base: "/HonestyEngineeringWorks/", // Set base path
+  build: {
+    outDir: "dist"
+  }
+});
