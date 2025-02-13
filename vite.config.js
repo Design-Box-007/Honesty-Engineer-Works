2a12dc840ddfcb544e8cdedd4f2e15d4037dafbe
Akram-00 <aliakram9789@gmail.com>
2025-02-13 12:46:24 +0530

    viteconfig is changed

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,20 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/HonestyEngineeringWorks/", // Set base path
+  base: "/", // Set base path
   build: {
     outDir: "dist"
   }
 });
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/", // Set base path
+//   build: {
+//     outDir: "dist"
+//   }
+// });
+
