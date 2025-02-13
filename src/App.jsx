60ec4986b0e800a859a6e98ae32e2839840ca923
Akram-00 <aliakram9789@gmail.com>
2025-02-13 12:41:09 +0530

    basename is changed

--- src/App.jsx
+++ src/App.jsx
@@ -11,18 +11,18 @@ import Furniture from './pages/Furniture';
 function App() {
 
   return (
-     <div className='global-style'>
-      <HashRouter>
-      <Header/>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/services' element={<Services/>} />
-        <Route path='/furniture' element={<Furniture/>} />
-        <Route path='/contact' element={<ContactSection/>} />
-      </Routes>
-      <Footer/>
+    <div className='global-style'>
+      <HashRouter basename="/">
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/furniture' element={<Furniture />} />
+          <Route path='/contact' element={<ContactSection />} />
+        </Routes>
+        <Footer />
       </HashRouter>
-      </div>
+    </div>
   )
 }
 
